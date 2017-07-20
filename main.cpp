6f066855bcd0f50a843055bff3ff83d969c5ce89
AlexHCC <alexander06088@yahoo.com>
2017-07-20 18:46:16 +0300

    Map values to grid

--- main.cpp
+++ main.cpp
@@ -10,11 +10,13 @@ sf::Image fractal;
 sf::Texture draw;
 sf::Sprite print;
 
+void mapValues(float posX, float posY, int &linkX, int &linkY, float canvasSize);
+
 int main(int argc, char** argv) {
     
     //Initialization
     if (!draw.create(WIDTH, HEIGHT)) {return -1;}
-    fractal.create(WIDTH, HEIGHT, sf::Color(100, 30, 30));
+    fractal.create(WIDTH, HEIGHT, sf::Color(30, 30, 30));
     
     cout << "Mandelbrot set" << endl;
     sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Mandelbrot set", sf::Style::Titlebar | sf::Style::Close);
@@ -27,11 +29,17 @@ int main(int argc, char** argv) {
                 window.close();
         }
         
-        for(int i = 0; i < 800; i++) {fractal.setPixel(i, i, sf::Color(0, 30, 100));}
         
-        draw.update(fractal);
-              
-        window.clear(sf::Color(30, 30, 30));
+        
+        //for(int i = 0; i < 800; i++) {fractal.setPixel(i, i, sf::Color(255, 255, 0));}
+        
+        int x, y = 0;
+        
+        mapValues(0.5555555f, 1.99999999999f, x, y, 2.0f);
+        fractal.setPixel(x, y, sf::Color(255, 255, 0));
+        
+        draw.update(fractal);     
+        window.clear();
         print.setTexture(draw, false);
         window.draw(print);
         window.display();
@@ -40,3 +48,11 @@ int main(int argc, char** argv) {
     return 0;
 }
 
+void mapValues(float posX, float posY, int &linkX, int &linkY, float canvasSize) {
+    if (posX > canvasSize || posX < -canvasSize || posY > canvasSize || posY < -canvasSize) {
+        cout << "Out of bounds map values" << endl;
+        return;
+    }
+    linkX = ((int)(WIDTH / 2 + (posX * WIDTH / 2) / canvasSize));
+    linkY = ((int)(HEIGHT / 2 - (posY * HEIGHT / 2) / canvasSize));
+}
