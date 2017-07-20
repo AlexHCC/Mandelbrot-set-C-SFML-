050e94d750a50c3c1765e027c09f8aeb4b5d9111
AlexHCC <alexander06088@yahoo.com>
2017-07-20 23:05:22 +0300

    Colors and move/zoom ability

--- main.cpp
+++ main.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
 #include <cmath>
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 
 using namespace std;
 
 //Variables
-const int WIDTH =  800, HEIGHT = 800;
+const int WIDTH =  400, HEIGHT = 400;
 sf::Image fractal;
 sf::Texture draw;
 sf::Sprite print;
-int numOfIterations = 500;
+int numOfIterations = 100;
+float scale = 2.0f;
+float translation = 0.1f;
+float xOffset = 0.0f;
+float yOffset = 0.0f;
 
 void mapValues(int posX, int posY, float &linkX, float &linkY, float canvasSize);
+void moveCanvas();
+sf::Color hsv(int hue, float sat, float val);
 
 int main(int argc, char** argv) {
     
@@ -28,28 +35,31 @@ int main(int argc, char** argv) {
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {xOffset = xOffset - translation*scale;}
+            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {xOffset = xOffset + translation*scale;}
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {yOffset = yOffset - translation*scale;}
+            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {yOffset = yOffset + translation*scale;}
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Add)) {scale = scale * 0.9;}
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Subtract)) {scale = scale / 0.9;}
         }
         
         for (int x = 0; x < WIDTH; x++) { for (int y = 0; y < HEIGHT; y++) {
             float a, b;
-            mapValues(x, y, a, b, 1.70f);
+            mapValues(x, y, a, b, scale);
             float tx = 0.0f;
             float ty = 0.0f;
+            a = a + xOffset;
+            b = b + yOffset;
             
             for (int i = 0; i < numOfIterations; i++) {
                 float xtemp =  tx*tx - ty*ty + a;
                 ty = 2*tx*ty + b;
                 tx = xtemp;
-                if (tx*tx + ty*ty >= 2*2) {fractal.setPixel(x, y, sf::Color(i, i, i)); i = numOfIterations;}
+                if (tx*tx + ty*ty >= 2*2) {fractal.setPixel(x, y, hsv(i, 255, 255)); i = numOfIterations;}
                 else {fractal.setPixel(x, y, sf::Color(255, 255, 255));}
             }
            
         }}
-        
-        //int x, y = 0;
-        //mapValues(0.5555f, 1.99999f, x, y, 2.0f);
-        //fractal.setPixel(x, y, sf::Color(255, 255, 0));
-        //for(int i = 0; i < 800; i++) {fractal.setPixel(i, i, sf::Color(255, 255, 0));}
         
         draw.update(fractal);     
         window.clear();
@@ -71,7 +81,39 @@ void mapValues(int posX, int posY, float &linkX, float &linkY, float canvasSize)
     
     linkX = (posX - WIDTH / 2) * canvasSize / WIDTH * 2;
     linkY = (posY - HEIGHT / 2) * canvasSize / HEIGHT * 2;
-    linkX = linkX - 0.5f;
+    //linkX = linkX - 0.5;
     //linkY = linkY - 0.5f;
     
+}
+
+void moveCanvas() {
+    
+}
+
+sf::Color hsv(int hue, float sat, float val) {
+    hue %= 360;
+    while(hue<0) hue += 360;
+
+    if(sat<0.f) sat = 0.f;
+    if(sat>1.f) sat = 1.f;
+
+    if(val<0.f) val = 0.f;
+    if(val>1.f) val = 1.f;
+
+    int h = hue/60;
+    float f = float(hue)/60-h;
+    float p = val*(1.f-sat);
+    float q = val*(1.f-sat*f);
+    float t = val*(1.f-sat*(1-f));
+
+    switch(h) {
+    default:
+        case 0:
+        case 6: return sf::Color(val*255, t*255, p*255);
+        case 1: return sf::Color(q*255, val*255, p*255);
+        case 2: return sf::Color(p*255, val*255, t*255);
+        case 3: return sf::Color(p*255, q*255, val*255);
+        case 4: return sf::Color(t*255, p*255, val*255);
+        case 5: return sf::Color(val*255, p*255, q*255);
+    }
 }
